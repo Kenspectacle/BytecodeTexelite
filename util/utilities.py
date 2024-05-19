43310b1af607cb3f513d5cb283bd7676cdeeb090
Kenspectacle <Kennyftw2@gmail.com>
2024-05-19 10:17:10 +0200

    change: file counter now can filter directory with certain path name

--- util/utilities.py
+++ util/utilities.py
@@ -20,10 +20,16 @@ def find_total_files_with_certain_file_ending(file_ending, path):
 # Find all of the file with a certain file ending while filtering out a directory
 def find_total_files_with_file_ending_and_filtered_directory(file_ending, path, filtered_directory):
     total_files = 0
+    
     for dirpath, dirs, files in os.walk(path):
-        # Ignores StubClass folder
-        dirname = dirpath.split(os.path.sep)[-1]
+        
+        # Find the relative path and filter all the directory that is in the relative path
+        rel_path = os.path.relpath(dirpath, path)
+        if filtered_directory in rel_path:
+                    continue  # Skip this directory
+                
         for file in files:
-            if file.endswith(file_ending) and dirname != filtered_directory:
+            if file.endswith(file_ending):
                 total_files += 1
+                
     return total_files
