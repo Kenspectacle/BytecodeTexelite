3c90e32028ce29dbf2e1b2f0b4de3ecfd9937b3a
Kenspectacle <Kennyftw2@gmail.com>
2024-05-20 09:48:43 +0200

    removed: unnecessary old code

--- remove_all_file_except_bc_files.py
+++ remove_all_file_except_bc_files.py
@@ -16,24 +16,6 @@ def main():
     current_file_number = 0
     
     walk_with_filtered_file_ending(path, command, file_ending, total_files)
-    
-
-    # # Traverse through the directories(and subdirectories) in Java_Classes
-    # for dirpath, dirs, files in os.walk(path):
-    #     for file in files:
-    #         if file.endswith(".bc"):
-    #             print("File Progress: " + str(current_file_number) +
-    #                 "/" + str(total_files))
-    #             current_file_number += 1
-    #             continue
-    #         current_file_number += 1
-    #         os.chdir(dirpath)
-    #         print("File Progress: " + str(current_file_number) +
-    #                 "/" + str(total_files))
-    #         print(os.getcwd() + " " + file)
-    #         subprocess.run(command + "'" + file + "'", shell=True,
-    #                 stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
 
 if __name__ == "__main__":
     main()
