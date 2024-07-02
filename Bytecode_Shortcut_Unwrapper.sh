3858fa63fca9e41fe6f45505ba204a6ddefb4364
Kenspectacle <Kennyftw2@gmail.com>
2024-07-02 14:42:58 +0200

    refactor: added comment on what bytecode shortcut unwrapper does

--- Bytecode_Shortcut_Unwrapper.sh
+++ Bytecode_Shortcut_Unwrapper.sh
@@ -4,7 +4,7 @@ TARGET_FOLDER="$1"
 
 # Use find command to list all files recursively in the target folder
 find "$TARGET_FOLDER" -type f | while read -r file; do
-    # Use sed to remove the underscore
+    # Use sed to unwrap the shortcut
     sed -i 's/\([a-zA-Z_][a-zA-Z_0-9]*\)_\([0-9]\{1,\}\)/\1 \2/; s/iconst_m1/iconst -1/' "$file"
 done
 
